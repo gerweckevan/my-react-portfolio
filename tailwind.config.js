9e2a2036387ccc6a997820fc3b04df8313d590cd
Evan Gerweck <gerweckevan@gmail.com>
2025-10-12 17:31:17 -0600

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
 var defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx,html}','./pages/**/*.{js,jsx,ts,tsx}','./app/**/*.{js,jsx,ts,tsx,mdx}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,html}',
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx,ts,tsx,mdx}',
+  ],
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
     fontFamily: {
-      sans: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+      sans: ['Ubuntu'].concat(defaultTheme.fontFamily.sans),
     },
     extend: {
       backgroundImage: () => ({
         'app-background': "url('./Assets/Background.jpg')",
-      })
+      }),
     },
   },
   variants: {
@@ -19,3 +23,4 @@ module.exports = {
   },
   plugins: [],
 }
+
